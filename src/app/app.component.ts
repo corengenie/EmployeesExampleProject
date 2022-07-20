d103afdee98ea510106a258fe8a51c25debdd6e7
corengenie <shved1004@gmail.com>
2022-07-20 21:12:30 +0300

    Базовый набор компонентов и страница 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="container">
+              <app-navbar></app-navbar>
+              <router-outlet></router-outlet>
+            </div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes =[
+  { path: 'home', component: HomeComponent },
+  { path: 'employees', component: EmployeesComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EmployeesComponent,
+    NotfoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
